1272b581a73457bcdb6f4d5f16223c7fb7e9e0ce
Yeezuu-web <yazugaming18@gmail.com>
2024-03-12 02:35:32 +0700

    remove admin

--- admin/src/app/(auth)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import NoRequireAuth from '@/providers/auth-page';
-import React from 'react';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <NoRequireAuth>{children}</NoRequireAuth>;
-}
--- admin/src/providers/provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from '@/redux/store';
-
-export default function Provider({ children }: { children: ReactNode }) {
-  return <ReduxProvider store={store}>{children}</ReduxProvider>;
-}
